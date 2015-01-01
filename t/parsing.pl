0d3940e199993fd3d95838ceaad97486734749fa
Michael Hendricks <michael@ndrix.org>
2015-01-01 14:15:14 -0700

    Tests for when_generating//1 and when_parsing//1

--- t/parsing.pl
+++ t/parsing.pl
@@ -7,3 +7,23 @@
 
 'generate this'(fail) :-
     phrase(parsing, _, _).
+
+
+'when_parsing: parsing' :-
+    Text = `something goes here`,
+    phrase(when_parsing(X=parsed), Text, _),
+    X == parsed.
+
+'when_parsing: generating' :-
+    phrase(when_parsing(X=parsed), _, _),
+    var(X).
+
+
+'when_generating: parsing' :-
+    Text = `something goes here`,
+    phrase(when_generating(X=generated), Text, _),
+    var(X).
+
+'when_generating: generating' :-
+    phrase(when_generating(X=generated), _, _),
+    X == generated.
